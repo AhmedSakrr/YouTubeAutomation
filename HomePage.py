df54f02e9f4882a4600a3711b51e4a2f49ca1cc7
AnkitaRaiyani <37570894+AnkitaRaiyani@users.noreply.github.com>
2018-03-21 18:50:59 +0530

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -7,15 +7,15 @@ class HomePage(BasePage) :
     def login(self):
         try:
             logout = LogOutPage.LogOutPage(self.driver)
-            logout.log_out()
-            account_button2 = self.driver.find_element_by_id("com.google.android.youtube:id/mobile_topbar_avatar")
-            account_button2.click()
-            sleep(4)
+            homepage = logout.log_out()
+            homepage.click_on_account()
         except:
             print "logout unsuccessful"
 
     def click_on_account(self):
-        sleep(4)
         account_button = self.driver.find_element_by_id("com.google.android.youtube:id/mobile_topbar_avatar")
         account_button.click()
-        sleep(4)
+        print account_button.is_displayed()
+        if account_button.is_displayed():
+            account_button.click()
+
